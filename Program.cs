d11b1a8f079d74c7e6911e452393f21040bf98f1
Mati2206 <63560003+Mati2206@users.noreply.github.com>
2022-02-04 12:58:55 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Obliczanie_liczby_kontrolnej_EAN_13
+{
+    class Program
+    {
+        static void Main()
+        {
+            string EAN = Console.ReadLine();
+            int checkDigit = EAN[EAN.Length - 1] - '0';
+            string test = EAN.Remove(EAN.Length - 1);
+            int sum = 0;
+            for (int i = 0; i < test.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += test[i] - '0';
+                }
+                else
+                {
+                    sum += 3 * (test[i] - '0');
+                }
+            }
+            if (10 - (sum%10) == checkDigit)
+            {
+                Console.WriteLine("Kod EAN-13 jest poprawny");
+            }
+            else
+            {
+                Console.WriteLine("Kod EAN-13 jest niepoprawny");
+            }
+        }
+    }
+}
